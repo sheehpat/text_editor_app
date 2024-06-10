52a6d44e4e5439b5bdc4192a10115501edd7d381
Patrick Sheehan <patrick@Patricks-MacBook-Air.local>
2024-06-09 21:19:13 -0600

    updated editor

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 app.use(express.urlencoded({ extended: true }));
